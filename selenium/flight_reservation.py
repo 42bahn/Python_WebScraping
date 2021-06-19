90c981ca8c4cff3b808264dad97ea52c97b74496
42bahn <bbu0704@gmail.com>
2021-06-19 19:57:01 +0900

    google_movie

--- selenium/flight_reservation.py
+++ selenium/flight_reservation.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import csv
 
 browser = webdriver.Chrome("selenium\chromedriver.exe")
 browser.maximize_window()
@@ -18,13 +17,6 @@ browser.find_elements_by_link_text("27")[1].click()
 browser.find_element_by_xpath("//*[@id='recommendationList']/ul/li[1]").click()
 
 browser.find_element_by_link_text("항공권 검색").click()
-
-filename = "티켓.csv"
-f = open(filename, "w", encoding="utf-8-sig", newline="")
-writer = csv.writer(f)
-
-title = "N	종목명	현재가	전일비	등락률	액면가	시가총액	상장주식수	외국인비율	거래량	PER	ROE	토론실".split("\t")
-writer.writerow(title)
 
 try:
     tickets = WebDriverWait(browser, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "trip_result_list li")))
---
+++ selenium/google_movie.py
@@ -0,0 +1,51 @@
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
+
+url = "https://play.google.com/store/movies/top"
+
+browser = webdriver.Chrome("selenium\chromedriver.exe")
+browser.maximize_window()
+browser.get(url)
+
+interval = 2
+prev_height = browser.execute_script("return document.body.scrollHeight")
+while True:
+    browser.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+    time.sleep(interval)
+    current_height = browser.execute_script("return document.body.scrollHeight")
+    if prev_height == current_height:
+        print("스크롤 완료")
+        break
+    prev_height = current_height
+
+soup = BeautifulSoup(browser.page_source, "lxml")
+
+# 구글 영화 인기 차트 전체 목록 조회 - 영화 제목 추출 
+# 
+# movies = soup.find_all("div", attrs={"class":"uMConb"})
+# print("영화 인기 차트 - " + str(len(movies)))
+# print("====================================================================")
+# for movie in movies:
+#     title = movie.find("div", attrs={"class":"WsMG1c nnK0zc"}).attrs['title']
+#     print(title)
+
+
+# 구글 영화 인기 차트 중 할인 영화 목록 조회 - 할인 영화 정보 추출 (제목, 정가 및 세일가, 링크)
+movies = soup.find_all("div", attrs={"class":"uMConb"})
+print("할인 영화 인기 차트")
+print("=" * 100)
+
+for movie in movies:
+    if movie.find("span", attrs={"class":"SUZt4c djCuy"}):
+        title = movie.find("div", attrs={"class":"WsMG1c nnK0zc"}).attrs['title']
+        cost = movie.find("span", attrs={"class":"SUZt4c djCuy"}).get_text()
+        sale = movie.find("span", attrs={"class":"VfPpfd ZdBevf i5DZme"}).get_text()
+        link = "https://play.google.com" + movie.find("a", attrs={"class":"JC71ub"})['href']     
+        print(f"영화 제목 : {title}")
+        print(f"구입가 : {cost} -> {sale}")
+        print(f"링크 : {link}")
+        print("-" * 100)
+
+browser.quit()
