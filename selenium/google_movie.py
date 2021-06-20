a6283f88aa19b2f3d554bc29d0dfae54455bf9ab
42bahn <bbu0704@gmail.com>
2021-06-20 21:51:25 +0900

    estate

--- selenium/google_movie.py
+++ selenium/google_movie.py
@@ -1,4 +1,3 @@
-import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
@@ -9,7 +8,7 @@ options = webdriver.ChromeOptions()
 options.headless = True
 options.add_argument("window-size=1920x1080")
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36")
-browser = webdriver.Chrome(executable_path="selenium\chromedriver.exe", options=options)
+browser = webdriver.Chrome(executable_path="chromedriver.exe", options=options)
 browser.maximize_window()
 browser.get(url)
 
---
+++ selenium/selenium.md
@@ -0,0 +1,9 @@
+# selenium
+
+## [Selenium with Python](https://selenium-python.readthedocs.io/)
+
+    셀레늄(Selenium)은 웹 애플리케이션 테스트를 위한 포터블 프레임워크이다.
+
+    Selenium은 웹 브라우저의 자동화를 가능하게 하고 지원하는 다양한 도구와 라이브러리를 포함한 프로젝트이다.
+
+    Selenium의 핵심은 WebDriver 입니다. 이는 다양한 브라우저에서 호환 가능한 지시사항을 작성할 수 있는 인터페이스라 할 수 있다. 
